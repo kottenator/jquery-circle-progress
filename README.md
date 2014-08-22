d090756909d45376438eb02dfb459630c6e98816
Rostyslav Bryzgunov <kottenator@gmail.com>
2014-08-22 20:17:42 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 jquery-circle-progress
 ======================
+
+jQuery Plugin to draw animated circular progress bars like this:
+
+![image](http://i.imgur.com/zV5VUQG.png)
+
+Browsers support
+----------------
+It uses `<canvas>` which is supported by all modern browsers (including mobile browsers) 
+and Internet Explorer 9+.
+
+I have not implemented any fallback / polyfill for unsupported browsers yet 
+*(i.e. for Internet Explorer 8 and older / misc browsers)*.
+
+Usage
+-----
+
+```html
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="jquery-circle-progress/dist/circle-progress.js"></script>
+
+<div id="circle"></div>
+
+<script>
+    $('#circle').circleProgress({ value: 0.75, startColor: 'red', endColor: 'orange' })
+</script>
+```
+
+Options
+-------
+You should specify options like in usage example above.
+
+Here are all available options:
+
+| Option  | Default | Description |
+| ------------- | ------------- | ------------- |
+| **value** | 0 | Despite of default value, you should specify your own. This is the only required option. It should be in range: `0.0 ... 1.0` |
+| size | 100 | Size of the circle / canvas in pixels |
+| startColor | '#3aeabb' |  |
+| endColor | '#fdd250' |  |
+| animation | ```{
+    duration: 1200,
+    easing: 'circleProgressEase' // equal to EaseInOutCubic
+}``` | Animation config. See [jQuery Animations](http://api.jquery.com/animate/). You may also set it to `false` |
---
+++ dist/circle-progress.js
@@ -0,0 +1,123 @@
+/*
+ * jquery-circle-progress - jQuery Plugin to draw animated circular progress bars
+ *
+ * @author https://github.com/kottenator
+ * @version 0.5
+ */
+
+// Renamed ease-in-out-cubic
+$.easing.circleProgressEase = function(x, t, b, c, d) {
+    if ((t /= d / 2) < 1)
+        return c / 2 * t * t * t + b;
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
+};
+
+// Default options
+$.circleProgress = {
+    defaults: {
+        value: 0,
+        size: 100,
+        startColor: '#3aeabb',
+        endColor: '#fdd250',
+        animation: {
+            duration: 1200,
+            easing: 'circleProgressEase' // equal to EaseInOutCubic
+        }
+    }
+};
+
+/**
+ * Draw animated circular progress bar.
+ *
+ * Appends <canvas> to the element or updates already appended one.
+ *
+ * If animated, throws 3 events:
+ *
+ *   - circle-animation-start(event)
+ *   - circle-animation-progress(event, progress) - multiple; progress: from 0.0 to 1.0
+*   - circle-animation-end(event)
+ *
+ * @param options Example: { value: 0.75, size: 50, animation: false };
+ *                you may set any of default options;
+ *                `animation` may be set to false;
+ *                you may also use .circleProgress('widget') to get the canvas
+ */
+$.fn.circleProgress = function(options) {
+    if (options == 'widget')
+        return this.data('circle-progress');
+
+    options = $.extend({}, $.circleProgress.defaults, options);
+
+    return this.each(function() {
+        var el = $(this),
+            s = options.size,  // square size
+            v = options.value, // current value: from 0.0 to 1.0
+            r = s / 2,         // radius
+            t = s / 14;        // thickness
+
+        // Prepare canvas
+        var canvas = el.data('circle-progress');
+
+        if (!canvas) {
+            canvas = $('<canvas>').appendTo(el)[0];
+            el.data('circle-progress', canvas);
+        }
+
+        canvas.width = s;
+        canvas.height = s;
+        var ctx = canvas.getContext('2d');
+        var lg = ctx.createLinearGradient(0, 0, s, 0);
+        lg.addColorStop(0, options.startColor);
+        lg.addColorStop(1, options.endColor);
+        ctx.fillStyle = "rgba(0, 0, 0, .1)";
+
+        // Draw circle
+        if (options.animation)
+            _drawAnimated(v);
+        else
+            _draw(v);
+
+        function _draw(p) {
+            // Clear frame
+            ctx.clearRect(0, 0, s, s);
+
+            // Draw background circle
+            ctx.beginPath();
+            ctx.arc(r, r, r, -Math.PI, Math.PI);
+            ctx.arc(r, r, r - t, Math.PI, -Math.PI, true);
+            ctx.closePath();
+            ctx.fill(); // gray fill
+
+            // Draw progress arc
+            ctx.beginPath();
+            ctx.arc(r, r, r, -Math.PI, -Math.PI + Math.PI * 2 * p);
+            ctx.arc(r, r, r - t, -Math.PI + Math.PI * 2 * p, -Math.PI, true);
+            ctx.closePath();
+            ctx.save();
+            ctx.clip();
+            ctx.fillStyle = lg;
+            ctx.fillRect(0, 0, s, s); // gradient fill
+            ctx.restore();
+        }
+
+        function _drawAnimated(v) {
+            el.trigger('circle-animation-start');
+
+            $(canvas).animate(
+                {
+                    value: v
+                },
+                $.extend({}, options.animation, {
+                    step: function(p) {
+                        _draw(p);
+                        el.trigger('circle-animation-progress', [p / v]);
+                    },
+
+                    complete: function() {
+                        el.trigger('circle-animation-end');
+                    }
+                })
+            );
+        }
+    });
+};
