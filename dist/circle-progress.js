3f932a3f38338a30ec3edf8c877ec2293dc05d3d
Rostyslav Bryzgunov <kottenator@gmail.com>
2014-08-25 21:17:31 +0300

    Version number updated

--- dist/circle-progress.js
+++ dist/circle-progress.js
@@ -2,7 +2,7 @@
  * jquery-circle-progress - jQuery Plugin to draw animated circular progress bars
  *
  * @author https://github.com/kottenator
- * @version 0.6
+ * @version 0.6.0
  */
 
 $.circleProgress = {
