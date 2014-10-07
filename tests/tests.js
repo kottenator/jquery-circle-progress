af5e218ccd0acdb53a1bb389b93348788fd547a0
Rostyslav Bryzgunov <kottenator@gmail.com>
2014-10-07 13:26:20 +0300

    Developing Unit Tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,19 @@
 QUnit.test("Test empty circle with no options", function(assert) {
     if (Modernizr.canvas) {
-        var el = $('<div>').appendTo('#qunit-fixture').circleProgress(),
+        var el = $('<div>').appendTo('#qunit-fixture').circleProgress({ value: 0 }),
+            canvas = el.circleProgress('widget'),
+            $canvas = $(canvas),
             defaultSize = 100,
-            defaultThickness = parseInt(defaultSize / 14), // 7
-            canvas = el.circleProgress('widget');
+            defaultThickness = parseInt(defaultSize / 14); // 7
 
-        assert.equal(canvas.tagName.toLowerCase(), 'canvas', "method .circleProgress('widget') returns HTMLCanvasElement");
-        assert.equal($.circleProgress.defaults.size, defaultSize, "default size: 100 pixels");
-        assert.equal($.circleProgress.defaults.thickness, 'auto', "default thickness: 'auto' (i.e. 1/14 of size)");
-        assert.equal(pixelColor(canvas, 0, defaultSize / 2), 'rgba(0, 0, 0, 0.1)', "gray circle color: rgba(0, 0, 0, 0.1)");
-        assert.equal(pixelColor(canvas, defaultThickness - 1, defaultSize / 2), 'rgba(0, 0, 0, 0.1)', "inside the border (6, 50)");
-        assert.equal(pixelColor(canvas, defaultThickness + 1, defaultSize / 2), 'rgba(0, 0, 0, 0)', "outside the border (8, 50)");
+        assert.equal($.circleProgress.defaults.size, defaultSize, "Default circle size: 100 pixels");
+        assert.equal($.circleProgress.defaults.thickness, 'auto', "Default circle thickness: 'auto' (i.e. 1/14 of size)");
+        assert.equal(canvas.tagName.toLowerCase(), 'canvas', "Method .circleProgress('widget') returns HTMLCanvasElement");
+        assert.equal($canvas.width(), defaultSize, "Actual width: 100 pixels");
+        assert.equal($canvas.height(), defaultSize, "Actual height: 100 pixels");
+        assert.equal(pixelColor(canvas, 0, defaultSize / 2), 'rgba(0, 0, 0, 0.1)', "Gray circle color: rgba(0, 0, 0, 0.1)");
+        assert.equal(pixelColor(canvas, defaultThickness - 1, defaultSize / 2), 'rgba(0, 0, 0, 0.1)', "Inside the border (6, 50)");
+        assert.equal(pixelColor(canvas, defaultThickness + 1, defaultSize / 2), 'rgba(0, 0, 0, 0)', "Outside the border (8, 50)");
     }
 });
 
