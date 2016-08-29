555bd1cfdf109e7cd1358650c44532d7906ab794
Rostyslav Bryzgunov <kottenator@gmail.com>
2016-08-29 17:34:28 -0400

    AMD & CommonJS support implemented (#61)
    UMD template for jQuery plugins used.

--- dist/circle-progress.js
+++ dist/circle-progress.js
@@ -7,7 +7,34 @@
  * @licence MIT
  * @preserve
  */
-(function($) {
+// UMD factory - https://github.com/umdjs/umd/blob/d31bb6ee7098715e019f52bdfe27b3e4bfd2b97e/templates/jqueryPlugin.js
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function(root, jQuery) {
+      if (jQuery === undefined) {
+        // require('jQuery') returns a factory that requires window to
+        // build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop
+        // if it's defined (how jquery works)
+        if (typeof window !== 'undefined') {
+          jQuery = require('jquery');
+        } else {
+          jQuery = require('jquery')(root);
+        }
+      }
+      factory(jQuery);
+      return jQuery;
+    };
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+})(function($) {
   /**
    * Inner implementation of the circle progress bar.
    * The class is not exposed _yet_ but you can create an instance through jQuery method call.
@@ -530,4 +557,4 @@
       }
     });
   };
-})(jQuery);
+});
