17c4681506b3d6aed46fd65beacc998d84c19422
Rostyslav Bryzgunov <kottenator@gmail.com>
2017-01-08 19:39:56 -0500

    Fix UMD CommonJS export

--- dist/circle-progress.js
+++ dist/circle-progress.js
@@ -3,33 +3,21 @@
  * {@link http://kottenator.github.io/jquery-circle-progress/}
  *
  * @author Rostyslav Bryzgunov <kottenator@gmail.com>
- * @version 1.2.0
+ * @version 1.2.1
  * @licence MIT
  * @preserve
  */
 // UMD factory - https://github.com/umdjs/umd/blob/d31bb6ee7098715e019f52bdfe27b3e4bfd2b97e/templates/jqueryPlugin.js
-// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+// Uses AMD, CommonJS or browser globals to create a jQuery plugin.
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
+    // AMD - register as an anonymous module
     define(['jquery'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // Node/CommonJS
-    module.exports = function(root, jQuery) {
-      if (jQuery === undefined) {
-        // require('jQuery') returns a factory that requires window to
-        // build a jQuery instance, we normalize how we use modules
-        // that require this pattern but the window provided is a noop
-        // if it's defined (how jquery works)
-        if (typeof window !== 'undefined') {
-          jQuery = require('jquery');
-        } else {
-          jQuery = require('jquery')(root);
-        }
-      }
-      factory(jQuery);
-      return jQuery;
-    };
+    var jQuery = require('jquery');
+    factory(jQuery);
+    module.exports = jQuery;
   } else {
     // Browser globals
     factory(jQuery);
